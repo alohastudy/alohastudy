b6e99d320a76be2dbe9479941501004426faf060
Andrew Millard <akm.scout@gmail.com>
2019-04-10 01:37:19 -1000

    fixed admin display

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -21,8 +21,8 @@ class NavBar extends React.Component {
               <Menu.Item as={NavLink} activeClassName="active" exact to="/listSpots" key='listSpots'>List Spots</Menu.Item>]
         ) : ''}
         {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
-            <Menu.Item as={NavLink} activeClassName="active" exact to="/admin" key='admin'>Admin</Menu.Item>,
-            <Menu.Item as={NavLink} activeClassName="active" exact to="/adminSpots" key='adminSpots'>Admin</Menu.Item>
+            [<Menu.Item as={NavLink} activeClassName="active" exact to="/admin" key='admin'>Admin</Menu.Item>,
+            <Menu.Item as={NavLink} activeClassName="active" exact to="/adminSpots" key='adminSpots'>Admin Spots</Menu.Item>]
         ) : ''}
         <Menu.Item position="right">
           {this.props.currentUser === '' ? (
