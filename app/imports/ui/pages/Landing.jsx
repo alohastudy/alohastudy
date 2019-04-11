6f330cba29a1686486ff213ef9883390ace7ac54
David <david947@hawaii.edu>
2019-04-11 04:58:12 -1000

    drafted landing page

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,22 +1,48 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Card, Feed, List } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-        <Grid verticalAlign='middle' textAlign='center' container>
+        <div className='img-background'>
+          <Grid verticalAlign='top' textAlign='left' padded container centered>
 
-          <Grid.Column width={4}>
-            <Image size='small' circular src="/images/meteor-logo.png"/>
-          </Grid.Column>
+            <Grid.Column width={9}>
+              <Card centered fluid>
+                <Card.Content>
+                  <h1>Welcome to Aloha Study!</h1>
+                  <p>designed to provide students a listing of hidden study spots. In this site, you will be provided a
+                    listing with ratings, hours, capacity, accessibility, and capabilities of each study spot. It will
+                    also include a feature allowing students to give real-time feedback if it’s either used or not used
+                    with a timestamp.</p>
+                  <h1>How to use this site:</h1>
+                  <List bulleted>
+                    <List.Item>Go to spots page to view spots </List.Item>
+                    <List.Item>Go to spots page to view spots </List.Item>
+                    <List.Item>Go to spots page to view spots </List.Item>
+                    <List.Item>Go to spots page to view spots </List.Item>
+                  </List>
+                  <h1>Symbols to look out for</h1>
+                  <List ordered>
+                  </List>
+                </Card.Content>
+              </Card>
+            </Grid.Column>
 
-          <Grid.Column width={8}>
-            <h1>Welcome to this template</h1>
-            <p>Now get to work and modify this app!</p>
-          </Grid.Column>
+            <Grid.Column width={5}>
+              <Card right fluid>
+                <Card.Content>
+                  <h1>Hot Spots</h1>
+                  <Feed>
+                    cool stuff about bench
+                  </Feed>
+                </Card.Content>
+              </Card>
+            </Grid.Column>
 
-        </Grid>
+          </Grid>
+        </div>
     );
   }
 }
