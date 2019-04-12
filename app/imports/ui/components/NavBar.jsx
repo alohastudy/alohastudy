15cdfe1ecd290711024b55b78c5ac21f1e447338
David <david947@hawaii.edu>
2019-04-12 00:09:08 -1000

    in-progress implement add spots

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -20,7 +20,7 @@ class NavBar extends React.Component {
           <Image size='medium' src='/images/logo10.png'/>
         </Menu.Item>
         {this.props.currentUser ? (
-            [<Menu.Item as={NavLink}  activeClassName="active" exact to="/add" key='add' style={itemStyle}>Add Spots</Menu.Item>,
+            [<Menu.Item as={NavLink}  activeClassName="active" exact to="/addSpots" key='addSpots' style={itemStyle}>Add Spots</Menu.Item>,
               <Menu.Item as={NavLink} activeClassName="active" exact to="/listSpots" key='listSpots' style={itemStyle}>List Spots</Menu.Item>]
         ) : ''}
         {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
---
+++ app/imports/ui/pages/AddSpot.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { Spots, SpotSchema } from '/imports/api/spot/spot';
+import { Grid, Segment, Header } from 'semantic-ui-react';
+import AutoForm from 'uniforms-semantic/AutoForm';
+import TextField from 'uniforms-semantic/TextField';
+import SelectField from 'uniforms-semantic/SelectField';
+import SubmitField from 'uniforms-semantic/SubmitField';
+import HiddenField from 'uniforms-semantic/HiddenField';
+import ErrorsField from 'uniforms-semantic/ErrorsField';
+import { Bert } from 'meteor/themeteorchef:bert';
+import { Meteor } from 'meteor/meteor';
+
+/** Renders the Page for adding a document. */
+class AddSpot extends React.Component {
+
+  /** Bind 'this' so that a ref to the Form can be saved in formRef and communicated between render() and submit(). */
+  constructor(props) {
+    super(props);
+    this.submit = this.submit.bind(this);
+    this.insertCallback = this.insertCallback.bind(this);
+    this.formRef = null;
+  }
+
+  /** Notify the user of the results of the submit. If successful, clear the form. */
+  insertCallback(error) {
+    if (error) {
+      Bert.alert({ type: 'danger', message: `Add failed: ${error.message}` });
+    } else {
+      Bert.alert({ type: 'success', message: 'Add succeeded' });
+      this.formRef.reset();
+    }
+  }
+
+  /** On submit, insert the data. */
+  submit(data) {
+    const { name, quantity, condition } = data;
+    const owner = Meteor.user().username;
+    Spots.insert({ name, quantity, condition, owner }, this.insertCallback);
+  }
+
+  /** Render the form. Use Uniforms: https://github.com/vazco/uniforms */
+  render() {
+    return (
+        <Grid container centered>
+          <Grid.Column>
+            <Header as="h2" textAlign="center">Add Spots</Header>
+            <AutoForm ref={(ref) => { this.formRef = ref; }} schema={SpotSchema} onSubmit={this.submit}>
+              <Segment>
+                <TextField name='name'/>
+                <TextField name='description'/>
+                <SelectField placeholder='hello' name='outlets'/>
+                <SelectField name='noisiness'/>
+                <SelectField name='location'/>
+                <SubmitField value='Submit'/>
+                <ErrorsField/>
+                <HiddenField name='owner' value='fakeuser@foo.com'/>
+              </Segment>
+            </AutoForm>
+          </Grid.Column>
+        </Grid>
+    );
+  }
+}
+
+export default AddSpot;
