3fbce180feb72f352abc07544eb2429a90bd779c
ilee72 <46728285+ilee72@users.noreply.github.com>
2019-04-30 10:46:07 -1000

    juice

--- app/imports/ui/components/SpotItem.jsx
+++ app/imports/ui/components/SpotItem.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Card, Image, Segment } from 'semantic-ui-react';
+import { Card, Image, Segment } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { withRouter, Link } from 'react-router-dom';
 import Rating from '/imports/ui/components/Rating';
