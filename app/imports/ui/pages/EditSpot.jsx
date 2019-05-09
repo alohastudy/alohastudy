395f347eb9b584fc1faeeec9c25bc111d0e89ddf
Andrew Millard <akm.scout@gmail.com>
2019-05-08 15:13:24 -1000

    fixed editspot

--- app/imports/ui/pages/EditSpot.jsx
+++ app/imports/ui/pages/EditSpot.jsx
@@ -72,7 +72,7 @@ export default withTracker(({ match }) => {
   // Get the documentID from the URL field. See imports/ui/layouts/App.jsx for the route containing :_id.
   const documentId = match.params._id;
   // Get access to Stuff documents.
-  const subscription = Meteor.subscribe('SpotVerified');
+  const subscription = Meteor.subscribe('SpotAll');
   return {
     doc: Spots.findOne(documentId),
     ready: subscription.ready(),
